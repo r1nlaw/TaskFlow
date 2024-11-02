fd7482090833bd682f8f4448efe15ef5c0087b22
r1nlaw <gru.zhizhi@mail.ru>
2024-11-02 17:01:19 +0300

    Добавил viper

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,27 @@ import (
 	"TaskFlow/package/repository"
 	"TaskFlow/package/service"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(taskflow.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running http setver: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
